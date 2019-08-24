ab9e4ed66acffc9b6ee26d7e937b12097f66f37c
Chris Garcia <cgarcia865@gmail.com>
2019-08-23 20:53:31 -0500

    Set Wedding date for timer

--- vendors/flipclock/timer.js
+++ vendors/flipclock/timer.js
@@ -1,10 +1,10 @@
 var today = new Date();
 
 var timer = function() {};
-timer.countdownDate = new Date();
+timer.countdownDate = new Date(2020, 10, 24, 18, 00, 00, 00);
 
 // set date to 10 days in the future for testing purposes
-timer.countdownDate.setDate( timer.countdownDate.getDate() + 10 );
+//timer.countdownDate.setDate( timer.countdownDate.getDate() + 10 );
 
 /*
 * Get thing started
